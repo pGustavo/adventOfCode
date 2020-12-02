9ebb8c08d7502f0961059adef2b008f30b00ca41
gusta <gustavo_pss@hotmail.com>
2020-12-02 20:15:51 +0000

    Day1 completed!

--- day1/day1.js
+++ day1/day1.js
@@ -1,31 +1,30 @@
 const fs = require("fs");
 const text = fs.readFileSync("day1/data.txt").toString('utf-8');
 const textByLine = text.split("\n");
+const arrayNumber =  textByLine.map(s => +s);
 
-let test = [];
-let result = null
-let number1 = null
-let number2 = null
+function rec() {
+    let countA = 0;
 
-function sum(arr, pos) {
-    for(let i = pos; i < arr.length - pos; i++) {
-        test.push(arr[i] + arr[i+1]);
-        if (arr[i] + arr[i+1] === 2020) {
-            number1 = arr[i];
-            number2 = arr[i+1];
-            result = arr[i] * arr[i+1];
-            console.log(result);
-        } else {
-            arr.slice(1);
+    while (countA <= arrayNumber.length) {
+        let firstA = arrayNumber[countA];
+        let countB = 1;
+
+
+        while (countB <= arrayNumber.length) {
+            let firstB = arrayNumber[countB];
+            if (firstA + firstB === 2020) {
+                console.log(firstA);
+                console.log(firstB);
+                console.log(firstA + firstB);
+                console.log(firstA * firstB);
+                return;
+            }
+            countB++;
         }
+        countA++;
     }
 }
 
-function recursive(arr) {
-    arr.forEach(number => {
-     sum(arr, arr.indexOf(number));
-    });
-    console.log('sums made: ', test.length);
-}
+rec();
 
-recursive(textByLine);
